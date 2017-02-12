aabecb2403473ff4be175c49ff36437ab32f9f0d
Jim Wilson <jamesmattheywilson@gmail.com>
2017-02-11 22:52:22 -0500

    pod crap

---
+++ FacePay/CustomObject.h
@@ -0,0 +1,23 @@
+//
+//  CustomObject.h
+//  FacePay
+//
+//  Created by James Wilson on 2/11/17.
+//  Copyright © 2017 jimdanger. All rights reserved.
+//
+
+#ifndef CustomObject_h
+#define CustomObject_h
+
+
+#endif 
+/* CustomObject_h */
+#import <Foundation/Foundation.h>
+
+@interface CustomObject: NSObject
+
+    @property (strong, nonatomic) id someProperty;
+
+- (void) someMethod;
+    
+    @end
---
+++ FacePay/Session.swift
@@ -0,0 +1,21 @@
+//
+//  Session.swift
+//  FacePay
+//
+//  Created by James Wilson on 2/11/17.
+//  Copyright © 2017 jimdanger. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+public class Session {
+
+    /// I'm just putting all kinds of info in this global singelton. Probbaly bad practice but hackathon so whatever.
+
+    open static var instance: Session  = Session()
+    
+    var user: FIRUser? // Firebase user
+
+
+}
